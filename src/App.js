d9216b3ecf7422466ff5cddea3b3072470cc9bca
Carlos Azaustre <cazaustre@gmail.com>
2017-02-04 14:32:25 +0100

    Paso 05 - Añadir logout y eventos

--- src/App.js
+++ src/App.js
@@ -1,18 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import firebase from 'firebase';
+import logo from './t3chfy_cmyk.png';
 import './App.css';
 
+
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      user: null
+    };
+
+    this.handleAuth = this.handleAuth.bind(this);
+  }
+
+  componentWillMount () {
+    // Cada vez que el método 'onAuthStateChanged' se dispara, recibe un objeto (user)
+    // Lo que hacemos es actualizar el estado con el contenido de ese objeto.
+    // Si el usuario se ha autenticado, el objeto tiene información.
+    // Si no, el usuario es 'null'
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ user });
+    });
+  }
+
+  handleAuth () {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} ha iniciado sesión`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+  handleLogout () {
+    firebase.auth().signOut()
+      .then(result => console.log(`${result.user.email} ha iniciado sesión`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+  renderLoginButton () {
+    if (!this.state.user) {
+      return (
+        <button onClick={this.handleAuth} className="App-btn">
+          Iniciar sesión con Google
+        </button>
+      );
+    } else  {
+      return (
+        <div className="App-intro">
+          <p className="App-intro">¡Hola, { this.state.user.displayName }!</p>
+          <button onClick={this.handleLogout} className="App-btn">
+            Salir
+          </button>
+        </div>
+
+      );
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>T3chfest 2017</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        { this.renderLoginButton() }
       </div>
     );
   }
