88360dc40338cb8f63f03d0106c24b15e5e6bc02
Carlos Azaustre <cazaustre@gmail.com>
2017-02-04 13:18:45 +0100

    Paso 03 - Añadir Firebase

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
+
 import App from './App';
 import './index.css';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA2r-MouLjwaR7KUyMRi8K5OCED1bE7BAw",
+  authDomain: "t3chfest-b3577.firebaseapp.com",
+  databaseURL: "https://t3chfest-b3577.firebaseio.com",
+  storageBucket: "t3chfest-b3577.appspot.com",
+  messagingSenderId: "714780734095"
+});
 
 ReactDOM.render(
   <App />,
